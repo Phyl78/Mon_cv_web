52de369081833358d476a177ad8e7333561a86dc
Phyl <phylchrist@gmail.com>
2021-10-20 12:25:56 +0200

    Progression sur la réalisation des pages

--- cvweb.js
+++ cvweb.js
@@ -20,3 +20,72 @@ window.addEventListener("scroll", ()=>{
 
     }
 })
+
+
+const txtAnim=document.querySelector('h5');
+
+ new Typewriter( txtAnim, {
+    //  loop:true,
+     deleteSpeed: 20
+ })
+
+.changeDelay(20)
+.typeString('Bonjour, moi c\'est Phyl')
+.pauseFor(300)
+.typeString('<strong>, Développeur web Front-End </strong> sur les langages :')
+.pauseFor(1000)
+.typeString('  <span style="color:maroon"> Html </span>')
+.pauseFor(1000)
+.deleteChars(5)
+.typeString('<span style="color:blue"> CSS </span>')
+.pauseFor(1000)
+.deleteChars(4)
+.typeString('<span style="color:yellow"> Javascript </span>')
+.pauseFor(1000)
+.deleteChars(11)
+.typeString('<span style="color:violet"> Php </span>')
+.pauseFor(1000)
+.deleteChars(4)
+.typeString('<span style="color:mediumslateblue"> Bootstrap </span>')
+.pauseFor(1000)
+.deleteChars(10)
+.typeString('<span style="color:blue"> Wordpress </span>')
+.pauseFor(1000)
+.deleteChars(10)
+.typeString('et <span style="color:green">  VBA sous Excel  </span>!')
+.pauseFor(1200)
+.deleteChars(49)
+.typeString('je recherche une alternance en <span style ="color : red"> Dev Front-end </span>')
+.pauseFor(1200)
+.deleteChars(18)
+.typeString('ou')
+.pauseFor(1000)
+.deleteChars(3)
+.typeString('  en <span style ="color : midnightblue">  Design UI/UX </span> à partir d\'octobre 2021 ! ')
+
+.start()
+
+
+
+// var clare = document.getElementById('alor'); 
+
+// clare.addEventListener('mouseover', function(e){
+//     this.style.border= "2px solid blue";
+// });
+
+// clare.addEventListener('mouseout', function(e){
+//     this.style.border= "none";
+// });
+
+
+
+// var clare = document.getElementById('alor'); 
+
+// clare.addEventListener('mouseover', function(e){
+//     this.style.border= "2px solid grey";
+// });
+
+// clare.addEventListener('mouseout', function(e){
+//     this.style.border= "none";
+// });
+
