34f6bbc9894034c0e39afe2a1ded14db8aaf1a4f
Phyl <phylchrist@gmail.com>
2021-10-23 20:02:13 +0200

    insertion des animations

--- cvweb.js
+++ cvweb.js
@@ -62,8 +62,99 @@ const txtAnim=document.querySelector('h5');
 .pauseFor(1000)
 .deleteChars(3)
 .typeString('  en <span style ="color : midnightblue">  Design UI/UX </span> à partir d\'octobre 2021 ! ')
-
 .start()
+
+// animations des expériences professionnelles
+
+const carte1 = document.querySelector('.exp1')
+const caceis= document.querySelector('.im1')
+const friedland = document.querySelector('.im2')
+const carte2= document.querySelector('.exp2')
+const carte3 = document.querySelector('.exp3')
+const grdf = document.querySelector('.im3')
+
+// animations des formations
+
+const carte4 = document.querySelector('.child1')
+const lamanu1 = document.querySelector('.child2')
+const lamanu2 = document.querySelector('.child3')
+const carte5 = document.querySelector('.child4')
+const carte6 = document.querySelector('.child5')
+const iae = document.querySelector('.child6')
+const carte7 = document.querySelector('.child8')
+const uca = document.querySelector('.child7')
+
+
+// animations langue et certification 
+
+const langue = document.querySelector('.toeic')
+const certif = document.querySelector('.amf')
+
+
+const TL1 = new TimelineMax({paused: false}); 
+
+TL1
+.from (carte1,1,{opacity:0, x:-100})
+.from (caceis,1,{opacity:0, x:100})
+.from (friedland,1,{opacity:0, x:-100})
+.from (carte2,1,{opacity:0, x:100})
+.from (carte3,1,{opacity:0, x:-100})
+.from (grdf,1,{opacity:0, x:100})
+
+
+const TL2 = new TimelineMax({paused: false});
+
+TL2
+.from (carte4,1,{opacity:0, x:-100})
+.from (lamanu1,1,{scale:0}, '-=0.4') 
+.from (lamanu2,1,{scale:0}, '-=0.4') 
+.from (carte5,1,{opacity:0, x:100},'-=0.4')
+.from (carte6,1,{opacity:0, x:-100}, '-=0.4')
+.from (iae,1,{scale:0},'-=0.4')
+.from (uca,1,{scale:0},'-=0.4')
+.from (carte7,1,{opacity:0, x:100},'-=0.4')
+
+
+const TL3 = new TimelineMax({paused: false});
+
+TL3
+.from (langue,1,{opacity:0, y:-100})
+.from (certif,1,{opacity:0 , y:100})
+
+
+// animation de la barre de navigation 
+
+const menu = document.querySelectorAll('#alor')
+
+
+const TL4 = new TimelineMax({paused:false})
+
+TL4
+
+.staggerFrom(menu,0.5, {scale:-2})
+
+
+// animations informations d'adresse 
+
+const fas1 = document.querySelectorAll('.fa1')
+const fas2 = document.querySelectorAll('.fa2')
+const fas3 = document.querySelectorAll('.fa3')
+
+
+const TL5 = new TimelineMax({paused:false})
+
+TL5
+.from (fas1,{opacity:0, x:-100})
+.from (fas2,1,{opacity:0, x:100})
+.from (fas3, 1,{opacity:0, x:-100})
+
+
+// .from (menu1,2,{opacity:0, y:-100})
+// .from (menu2,1.6, {opacity:0,y:-100}, '-=0.8.5' )
+// .from (menu3,1.2,{opacity:0, y:-100}, '-=0.6' )
+// .from (menu4,1,{opacity:0, y:-100}, '-=0.3' )
+// .from (menu5,0.8,{opacity:0, y:-100}, '-=0.1' )
+
 
 
 
